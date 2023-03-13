b1554acd99641b793744d9998f03e663545626c9
Anoter <prosidegcr@gmail.com>
2023-03-13 10:49:37 -0300

    Created a new method to cast the parameter and call the private one

--- lista2/ApenasCaracteres/src/Binaria.java
+++ lista2/ApenasCaracteres/src/Binaria.java
@@ -1,9 +1,15 @@
 import java.util.Stack;
 
 public class Binaria {
-    public String baseBinaria(Stack<Integer> binary, int input){ 
+
+    public String baseBinaria(String input) {
+        Stack<Integer> binary = new Stack<Integer>();
+        return baseBinariaRecusiva(binary, Integer.parseInt(input));
+    }
+
+    private String baseBinariaRecusiva(Stack<Integer> binary, int input){ 
         if(input <= 0) return binary.toString().replaceAll("[^0-9]", "");
         int i = input % 2 == 0 ? binary.push(0) : binary.push(1); // "i" apenas para comparação funcionar
-        return baseBinaria(binary, input = input / 2); // Recusividade para demais binarios
+        return baseBinariaRecusiva(binary, input = input / 2); // Recusividade para demais binarios
     }
 }
