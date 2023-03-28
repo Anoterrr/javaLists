730142dc104d5d5b1e16e6e43c3b4ea48c5f4ee2
Anoter <prosidegcr@gmail.com>
2023-03-28 14:45:02 -0300

    Addind palavra

---
+++ vetores/vetores/src/Palavra.java
@@ -0,0 +1,11 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Palavra {
+  char[] vogal;
+  char[] consonant;
+
+  private void setVogal() {
+    //for(int i = 0; i < )
+  } 
+}
